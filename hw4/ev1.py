7a3afae431dc0b83c0f3494ef391e724eb7c9629
Peter <841111chris@gmail.com>
2021-10-17 20:39:56 +0800

    new commit

--- hw4/ev1.py
+++ hw4/ev1.py
@@ -13,132 +13,153 @@ import sys
 import yaml
 import math
 from random import Random
+import numpy as np
+import matplotlib.pyplot as plt
 
 
-#EV1 Config class 
+# EV1 Config class
 class EV1_Config:
     """
     EV1 configuration class
     """
     # class variables
-    sectionName='EV1'
-    options={'populationSize': (int,True),
-             'generationCount': (int,True),
-             'randomSeed': (int,True),
-             'minLimit': (float,True),
-             'maxLimit': (float,True),
-             'mutationProb': (float,True),
-             'mutationStddev': (float,True)}
+    sectionName = 'EV1'
+    options = {'populationSize': (int, True),
+               'generationCount': (int, True),
+               'randomSeed': (int, True),
+               'minLimit': (float, True),
+               'maxLimit': (float, True),
+               'mutationProb': (float, True),
+               'mutationStddev': (float, True)}
      
-    #constructor
-    def __init__(self, inFileName):
-        #read YAML config and get EC_Engine section
-        infile=open(inFileName,'r')
-        ymlcfg=yaml.safe_load(infile)
+    # constructor
+    def __init__(self, in_file_name):
+        # read YAML config and get EC_Engine section
+        infile = open(in_file_name, 'r')
+        ymlcfg = yaml.safe_load(infile)
         infile.close()
-        eccfg=ymlcfg.get(self.sectionName,None)
-        if eccfg is None: raise Exception('Missing EV1 section in cfg file')
+        eccfg = ymlcfg.get(self.sectionName, None)
+        if eccfg is None:
+            raise Exception('Missing EV1 section in cfg file')
          
-        #iterate over options
+        # iterate over options
         for opt in self.options:
             if opt in eccfg:
-                optval=eccfg[opt]
+                optval = eccfg[opt]
  
-                #verify parameter type
+                # verify parameter type
                 if type(optval) != self.options[opt][0]:
                     raise Exception('Parameter "{}" has wrong type'.format(opt))
                  
-                #create attributes on the fly
-                setattr(self,opt,optval)
+                # create attributes on the fly
+                setattr(self, opt, optval)
             else:
                 if self.options[opt][1]:
                     raise Exception('Missing mandatory parameter "{}"'.format(opt))
                 else:
-                    setattr(self,opt,None)
+                    setattr(self, opt, None)
      
-    #string representation for class data    
+    # string representation for class data
     def __str__(self):
-        return str(yaml.dump(self.__dict__,default_flow_style=False))
+        return str(yaml.dump(self.__dict__, default_flow_style=False))
          
 
-#Simple 1-D fitness function example
+# Simple 1-D fitness function example
 #        
 def fitnessFunc(x):
-    return 50.0 - x*x
+    # return 50.0 - x*x
+    pi = np.pi
+    return -10 - np.square(0.04*x) + 10*np.cos(0.04*pi * x)
 
 
-#Find index of worst individual in population
-def findWorstIndex(l):
-    minval=l[0].fit
-    imin=0
-    for i in range(len(l)):
-        if l[i].fit < minval:
-            minval=l[i].fit
-            imin=i
-    return imin
+# Find index of worst individual in population
+def findWorstIndex(lst):
+    min_val = lst[0].fit
+    i_min = 0
+    for i in range(len(lst)):
+        if lst[i].fit < min_val:
+            min_val = lst[i].fit
+            i_min = i
+    return i_min
 
 
-#Print some useful stats to screen
-def printStats(pop,gen):
-    print('Generation:',gen)
-    avgval=0
-    maxval=pop[0].fit
+# Print some useful stats to screen
+def printStats(pop, gen):
+    print('Generation:', gen)
+    avgval = 0
+    maxval = pop[0]
     for p in pop:
-        avgval+=p.fit
-        if p.fit > maxval: maxval=p.fit
-        print(str(p.x)+'\t'+str(p.fit))
+        avgval += p.fit
+        if p.fit > maxval.fit:
+            maxval = p
+        # print(str(p.x)+'\t'+str(p.fit))
     
-    print('Max fitness',maxval)
-    print('Avg fitness',avgval/len(pop))
+    print('Max fitness', maxval)
+    print('Avg fitness', avgval/len(pop))
     print('')
 
 
-#A trivial Individual class
+# A trivial Individual class
 class Individual:
-    def __init__(self,x=0,fit=0):
-        self.x=x
-        self.fit=fit
+    def __init__(self, x=0, fit=0):
+        self.x = x
+        self.fit = fit
+
+    def __str__(self):
+        return {'x': self.x, 'fit': self.fit}.__str__()
 
 
-#EV1: The simplest EA ever!
+# EV1: The simplest EA ever!
 #            
 def ev1(cfg):
     # start random number generator
-    prng=Random()
+    prng = Random()
     prng.seed(cfg.randomSeed)
     
-    #random initialization of population
-    population=[]
+    # random initialization of population
+    population = []
     for i in range(cfg.populationSize):
-        x=prng.uniform(cfg.minLimit,cfg.maxLimit)
-        ind=Individual(x,fitnessFunc(x))
+        x = prng.uniform(cfg.minLimit, cfg.maxLimit)
+        ind = Individual(x, fitnessFunc(x))
         population.append(ind)
         
-    #print stats    
-    printStats(population,0)
+    # print stats
+    printStats(population, 0)
 
-    #evolution main loop
+    #  evolution main loop
     for i in range(cfg.generationCount):
-        #randomly select two parents
-        parents=prng.sample(population,2)
+        # randomly select two parents
+        parents = prng.sample(population, 2)
 
-        #recombine using simple average
-        childx=(parents[0].x+parents[1].x)/2
+        # recombine using simple average
+        childx = (parents[0].x + parents[1].x)/2
         
-        #random mutation using normal distribution
+        # random mutation using normal distribution
         if prng.random() <= cfg.mutationProb:
-            childx=prng.normalvariate(childx,cfg.mutationStddev)
+            childx = prng.normalvariate(childx, cfg.mutationStddev)
             
-        #survivor selection: replace worst
-        child=Individual(childx,fitnessFunc(childx))
-        iworst=findWorstIndex(population)
+        # survivor selection: replace worst
+        child = Individual(childx, fitnessFunc(childx))
+        iworst = findWorstIndex(population)
         if child.fit > population[iworst].fit:
-            population[iworst]=child
+            population[iworst] = child
+        if i % 10 == 0:
+            print('plot call')
+            plot(population)
         
-        #print stats    
-        printStats(population,i+1)
-        
-        
+        # print stats
+        printStats(population, i+1)
+
+
+def plot(pop):
+    x = np.linspace(-120, 120, 200)
+    y = fitnessFunc(x)
+    plt.plot(x, y, 'g')
+    for i in range(len(pop)):
+        plt.plot(pop[i].x, pop[i].fit, 'bo')
+    plt.show()
+
+
 #
 # Main entry point
 #
@@ -156,17 +177,17 @@ def main(argv=None):
         parser.add_option("-d", "--debug", action="store_true", dest="debugMode", help="debug mode", default=False)
         (options, args) = parser.parse_args(argv)
         
-        #validate options
+        # validate options
         if options.inputFileName is None:
             raise Exception("Must specify input file name using -i or --input option.")
         
-        #Get EV1 config params
-        cfg=EV1_Config(options.inputFileName)
+        # Get EV1 config params
+        cfg = EV1_Config(options.inputFileName)
         
-        #print config params
+        # print config params
         print(cfg)
                     
-        #run EV1
+        # run EV1
         ev1(cfg)
         
         if not options.quietMode:                    
@@ -178,8 +199,8 @@ def main(argv=None):
             print_exc()
         else:
             print(info)
-    
+
 
 if __name__ == '__main__':
-    main()
-    
+    main(['-i', './ev1_example.cfg'])
+
