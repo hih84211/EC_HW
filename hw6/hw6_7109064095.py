c3df8dbf449d3d5f6dc62e969189f8e5592435f4
hih84211 <841111chris@gmail.com>
2021-11-01 18:03:19 +0800

    New commit

--- hw6/hw6_7109064095.py
+++ hw6/hw6_7109064095.py
@@ -3,6 +3,7 @@
 #
 import random
 from random import Random
+import random
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -16,6 +17,7 @@ fit_range = 40
 #
 prng = Random()
 prng.seed(123)
+
 
 # Let's suppose we have an imaginary problem that generates
 # integer fitness values on some fixed range.  We'll start by randomly
@@ -43,11 +45,20 @@ def plt_hist(pop, generation=0, bin_limit=fit_range):
 def binary_tournament(pop_in, prng):
     newPop = []
 
+    # 先將pop_in洗牌，再切割成兩兩一組，得到n輪對戰組合
     #
-    # It's your job to implement this binary tournament function!
-    #
+    random.shuffle(pop_in)
+    tour1 = [pop_in[i:i + 2] for i in range(0, len(pop_in), 2)]
+    random.shuffle(pop_in)
+    tour2 = [pop_in[i:i + 2] for i in range(0, len(pop_in), 2)]
 
-    # return newPop
+    for i in range(len(tour1)):
+        # 把各個對戰組合排序後，pop出第一個就是最大的
+        tour1[i].sort()
+        tour2[i].sort()
+        newPop.append(tour1[i].pop())
+        newPop.append(tour2[i].pop())
+
     return newPop
 
 
@@ -55,7 +66,7 @@ def binary_tournament(pop_in, prng):
 # to the initial population and plot the resulting fitness histograms.
 # This is somewhat like having a selection-only EA without any stochastic variation operators
 #
-'''for i in range(generations):
+for i in range(generations):
     print(pop)
     plt_hist(pop, i)
 
@@ -63,6 +74,10 @@ def binary_tournament(pop_in, prng):
 '''
 rng = Random()
 lst = [(rng.randint(0, 1023), rng.randint(0, 511)) for i in range(10)]
-
-print(lst)
-
+lst2 = [rng.randint(0, 100) for i in range(10)]
+lst2.sort(reverse=True)
+print(lst2)
+print([lst2[i:i + 2] for i in range(0, len(lst2), 2)])
+random.shuffle(lst2)
+print([lst2[i:i + 2] for i in range(0, len(lst2), 2)])
+'''
